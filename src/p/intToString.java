e246a04f998fac383a4e8216feda05c3e3d9ae43
Uj <Cujwal25@gmail.coom>
2022-11-12 23:43:25 +0530

    yyyy

---
+++ src/p/intToString.java
@@ -0,0 +1,14 @@
+package p;
+
+public class intToString {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		String w = Integer.toString(a);
+		System.out.println(w);
+		
+		
+		
+	}
+}
